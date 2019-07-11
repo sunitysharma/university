34e3f2e0f94fc605cb9f2781687ecbaacb4e2ac1
Sunity <sunity_hello@yahoo.co.in>
2019-07-11 11:50:02 +0530

    Slider and Hamburger Updated
    Slider
    Hamburger -dropdown

---
+++ js/script.js
@@ -0,0 +1,82 @@
+$('#menuBtn').click(function() {
+  $('#mainNav').toggleClass('active');
+})
+
+
+//slider jquery
+// $('#prevBtn').click(function() {})
+// $('#nextBtn').click(function() {})
+
+var updateSliderInterval;
+var startInterval = false;
+
+function updateSlider(id) {
+  startInterval = false;
+  if(id == 'nextBtn') {
+    // logic for next button
+    if($('.slider .images li:last-child').hasClass('active')) {
+      $('.slider .images li.active').removeClass('active');
+      $('.slider .indicators li.active').removeClass('active');
+      $('.slider .images li:first-child').addClass('active');
+      $('.slider .indicators li:first-child').addClass('active');
+    } else {
+      $('.slider .images li.active + li').addClass('temp-active');
+      $('.slider .indicators li.active + li').addClass('temp-active');
+      $('.slider .images li.active').removeClass('active');
+      $('.slider .indicators li.active').removeClass('active');
+
+      $('.slider .images li.temp-active').addClass('active');
+      $('.slider .indicators li.temp-active').addClass('active');
+      $('.slider .images li.temp-active').removeClass('temp-active');
+      $('.slider .indicators li.temp-active').removeClass('temp-active');
+    }
+  } else {
+    // logic for prev button
+    if($('.slider .images li:first-child').hasClass('active')) {
+      $('.slider .images li.active').removeClass('active');
+      $('.slider .images li:last-child').addClass('active');
+      $('.slider .indicators li.active').removeClass('active');
+      $('.slider .indicators li:last-child').addClass('active');
+    } else {
+      $('.slider .images li.active').prev().addClass('temp-active');
+      $('.slider .images li.active').removeClass('active');
+      $('.slider .indicators li.active').prev().addClass('temp-active');
+      $('.slider .indicators li.active').removeClass('active');
+
+      $('.slider .images li.temp-active').addClass('active');
+      $('.slider .images li.temp-active').removeClass('temp-active');
+      $('.slider .indicators li.temp-active').addClass('active');
+      $('.slider .indicators li.temp-active').removeClass('temp-active');
+    }
+  }
+}
+
+$('#prevBtn, #nextBtn').click(function() {
+  updateSlider(this.id)
+})
+
+function interval() {
+  updateSliderInterval = setInterval(function() {
+    updateSlider('nextBtn');
+  }, 10000);
+}
+interval();
+
+$('.slider').mouseenter(function() {
+  console.log('stop');
+  clearInterval(updateSliderInterval);
+})
+$('.slider').mouseout(function(e) {
+  console.log('start');
+  if(startInterval) {
+    interval();
+  }
+  startInterval = true;
+})
+
+$('.slider').mousemove(function(e) {
+  console.log();
+  if(e.target.classList.contains('btn')) {
+    startInterval = false;
+  }
+})
